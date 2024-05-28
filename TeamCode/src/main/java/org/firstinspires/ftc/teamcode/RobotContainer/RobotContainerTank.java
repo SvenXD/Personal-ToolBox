186d893c7234cbc73bdc77bab546462ae09e3f8f
SvenXD <Romanhernandezsosa1@gmail.com>
2024-05-27 19:48:58 -0600

    Added custom rotation for auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RobotContainer/RobotContainerTank.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.RobotContainer;
+
+import com.arcrobotics.ftclib.command.CommandOpMode;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.button.GamepadButton;
+import com.arcrobotics.ftclib.command.RunCommand;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Commands.TankDriveCommand;
+
+import org.firstinspires.ftc.teamcode.Subsystems.TankDriveSubsystem;
+import org.firstinspires.ftc.teamcode.drive.SampleTankDrive;
+
+@TeleOp
+public class RobotContainerTank extends CommandOpMode {
+    @Override
+    public void initialize() {
+        SampleTankDrive sampleTankDrive = new SampleTankDrive(hardwareMap);
+        TankDriveSubsystem m_drive = new TankDriveSubsystem(sampleTankDrive);
+
+        GamepadEx chassisDriver = new GamepadEx(gamepad1);
+        GamepadEx subsystemsDriver = new GamepadEx(gamepad2);
+
+        //Tank----------------------------------
+
+        m_drive.setDefaultCommand(new TankDriveCommand(m_drive, chassisDriver::getLeftY
+                ,chassisDriver::getRightX));
+
+        chassisDriver.getGamepadButton(GamepadKeys.Button.B)
+                .whenPressed(new InstantCommand(m_drive::bajarVel));
+
+        //------------------------------------------
+
+        schedule(new RunCommand(() -> {
+            m_drive.update();
+            telemetry.addData("Heading", m_drive.getPoseEstimate().getHeading());
+            telemetry.update();
+        }));
+//Test
+    }
+
+
+
+}
